63c983d93e21770df2707a27c08fa8709ae00790
Arda Özler <ardaozler@yahoo.com>
2025-11-16 13:18:37 +0100

    feat: added plc simulation

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,24 +1,137 @@
-﻿using System.Text;
+﻿using System.Diagnostics;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace Technoteam
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        private readonly PlcSimulator _plc = new();
+        private readonly DispatcherTimer _timer = new();
+        private readonly Stopwatch _stopwatch = new();
+        private bool _overheatFlash;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            _plc.Start();
+
+            ConfigureUIEvents();
+            ConfigureTimer();
+        }
+
+        private void ConfigureUIEvents()
+        {
+            EmergencyStopButton1.Click += (s, e) => EmergencyStopButton1_OnClick(s, e);
+            StartButton1.Click += (s, e) => StartButton1_OnClick(s, e);
+            TargetSpeedSlider1.ValueChanged += (s, e) => TargetSpeedSlider1_OnValueChanged(s, e);
+        }
+
+        private void TargetSpeedSlider1_OnValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            _plc.TargetSpeed = (int)TargetSpeedSlider1.Value;
+            TargetSpeedValue1.Text = $"{_plc.TargetSpeed} RPM";
+        }
+
+        private void StartButton1_OnClick(object sender, RoutedEventArgs e)
+        {
+            _plc.ManualStartRequested = true;
+        }
+
+        private void EmergencyStopButton1_OnClick(object sender, RoutedEventArgs e)
+        {
+            _plc.ManualEmergencyStopRequested = true;
+        }
+
+        private void ConfigureTimer()
+        {
+            _timer.Interval = TimeSpan.FromMilliseconds(100);
+            _timer.Tick += TimerOnTick;
+
+            _stopwatch.Start();
+            _timer.Start();
+        }
+
+        private void TimerOnTick(object? sender, EventArgs e)
+        {
+            var dt = _stopwatch.Elapsed.TotalSeconds;
+            _stopwatch.Restart();
+
+            _plc.Update(dt);
+
+            if (_plc.IsOverheatWarning)
+            {
+                _overheatFlash = !_overheatFlash;
+            }
+            else
+            {
+                _overheatFlash = false;
+            }
+
+            UpdateUiFromPlc();
+        }
+
+        private void UpdateUiFromPlc()
+        {
+            TempBar1.Value = _plc.Temperature;
+            if (_plc.Temperature >= PlcSimulator.TemperatureLimit)
+            {
+                TempBar1.Foreground = Brushes.Red;
+            }
+            else
+            {
+                TempBar1.Foreground = Brushes.Green;
+            }
+            TempValue1.Text = $"{_plc.Temperature} °C";
+
+            ActualSpeedBar1.Value = _plc.ActualSpeed;
+            ActualSpeedValue1.Text = $"{_plc.ActualSpeed} RPM";
+
+            if ((int)TargetSpeedSlider1.Value != _plc.TargetSpeed)
+            {
+                TargetSpeedSlider1.Value = _plc.TargetSpeed;
+            }
+            TargetSpeedValue1.Text = $"{_plc.TargetSpeed} RPM";
+
+            switch (_plc.State)
+            {
+                case PlcState.Normal:
+                    if (_plc.IsOverheatWarning)
+                    {
+                        StatusLight1.Fill = _overheatFlash ? Brushes.OrangeRed : Brushes.Yellow;
+                        StatusLabel1.Text = "Overheating!";
+                    }
+                    else
+                    {
+                        StatusLight1.Fill = Brushes.Green;
+                        StatusLabel1.Text = "Normal";
+                    }
+
+                    TargetSpeedSlider1.IsEnabled = true;
+                    StartButton1.IsEnabled = false;
+                    EmergencyStopButton1.IsEnabled = _plc.ActualSpeed > 0;
+                    break;
+
+                case PlcState.EmergencyStop:
+                    StatusLight1.Fill = Brushes.Red;
+                    StatusLabel1.Text = "Emergency Stop";
+
+                    TargetSpeedSlider1.IsEnabled = false;
+                    StartButton1.IsEnabled = _plc.Temperature < PlcSimulator.TemperatureLimit;
+                    EmergencyStopButton1.IsEnabled = false;
+                    break;
+
+                case PlcState.MaintenanceNeeded:
+                    StatusLight1.Fill = Brushes.Yellow;
+                    StatusLabel1.Text = "Maintenance Needed";
+
+                    TargetSpeedSlider1.IsEnabled = false;
+                    StartButton1.IsEnabled = false;
+                    EmergencyStopButton1.IsEnabled = false;
+                    break;
+            }
         }
     }
 }
---
+++ PlcSimulator.cs
@@ -0,0 +1,176 @@
+﻿using System.Diagnostics;
+
+namespace Technoteam
+{
+    public enum PlcState
+    {
+        Normal, //motor running normally
+        EmergencyStop, //motor mechanically stopped due to emergency set the actual speed to 0
+        MaintenanceNeeded //motor stopped due to overheating, needs maintenance, slow the actual speed to 0
+    }
+    public class PlcSimulator
+    {
+        public int Temperature { get; private set; } = 25;
+        public int ActualSpeed { get; private set; } = 0;
+        public PlcState State { get; private set; } = PlcState.Normal;
+
+        public int TargetSpeed { get; set; } = 0;
+        public bool ManualEmergencyStopRequested { get; set; } = false;
+        public bool ManualStartRequested { get; set; } = false;
+
+        public const int TemperatureLimit = 105;
+
+        private const int MaxSpeed = 2000;
+        private double _timeAboveTempLimit = 0;
+        private Random _rand = new Random(10); //TODO: remove the seed
+        private const int SpeedChangeRate = 100; //RPM per second
+        private const int EmergencyDecelerationRate = 500; //RPM per second
+        private const int AmbientTemperature = 25;
+        private int heatingThresholdRPM; //different for each plc, just random for simulation
+        private int _operatingTemperature;
+
+        public bool IsOverheatWarning => State == PlcState.Normal && Temperature > TemperatureLimit;
+
+        public void Start()
+        {
+            heatingThresholdRPM = _rand.Next(1200, 1900);
+            _operatingTemperature = _rand.Next(70, 90);
+        }
+
+        public void Update(double tDelta)
+        {
+            if (ManualEmergencyStopRequested)
+            {
+                EmergencyStopRequested();
+                ManualEmergencyStopRequested = false;
+            }
+
+            UpdateTemperature();
+            StateUpdate(tDelta);
+            UpdateSpeed(tDelta);
+        }
+
+        private void UpdateSpeed(double tDelta)
+        {
+            if (State == PlcState.EmergencyStop) //mechanical stop
+            {
+                ActualSpeed = 0;
+                return;
+            }
+
+            var rate = State == PlcState.MaintenanceNeeded //cut power bc overheat
+                ? EmergencyDecelerationRate
+                : SpeedChangeRate;
+
+            var speedDelta = rate * tDelta;
+
+            if (ActualSpeed < TargetSpeed)
+            {
+                ActualSpeed += (int)speedDelta;
+                if (ActualSpeed > TargetSpeed)
+                {
+                    ActualSpeed = TargetSpeed;
+                }
+            }
+            else if (ActualSpeed > TargetSpeed)
+            {
+                ActualSpeed -= (int)speedDelta;
+                if (ActualSpeed < TargetSpeed)
+                {
+                    ActualSpeed = TargetSpeed;
+                }
+            }
+        }
+
+        private void UpdateTemperature()
+        {
+            if (ActualSpeed > 0)
+            {
+
+                if (Temperature < _operatingTemperature - 2 || ActualSpeed >= heatingThresholdRPM)
+                {
+                    Temperature += _rand.Next(0, 3);
+                }
+                else if (Temperature > _operatingTemperature + 2)
+                {
+                    Temperature -= _rand.Next(1, 3);
+                }
+                else
+                {
+                    Temperature += _rand.Next(-1, 2);
+                }
+            }
+            else
+            {
+                if (Temperature > AmbientTemperature)
+                {
+                    Temperature -= _rand.Next(0, 2);
+                    if (Temperature < AmbientTemperature)
+                    {
+                        Temperature = AmbientTemperature;
+                    }
+                }
+                else if (Temperature < AmbientTemperature)
+                {
+                    Temperature++;
+                }
+            }
+        }
+
+        private void StateUpdate(double tDelta)
+        {
+            switch (State)
+            {
+                case PlcState.Normal:
+                    NormalStateUpdate(tDelta);
+                    break;
+                case PlcState.EmergencyStop:
+                    EmergencyStopStateUpdate();
+                    break;
+                case PlcState.MaintenanceNeeded:
+                    MaintenanceStateUpdate();
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void NormalStateUpdate(double tDelta)
+        {
+            if (Temperature > TemperatureLimit)
+            {
+                _timeAboveTempLimit += tDelta;
+                if (_timeAboveTempLimit >= 10)
+                {
+                    State = PlcState.MaintenanceNeeded;
+                    _timeAboveTempLimit = 0;
+                }
+            }
+            else
+            {
+                _timeAboveTempLimit = 0;
+            }
+        }
+
+        private void EmergencyStopStateUpdate()
+        {
+            TargetSpeed = 0;
+            if (ManualStartRequested && Temperature < TemperatureLimit)
+            {
+                State = PlcState.Normal;
+                ManualStartRequested = false;
+            }
+        }
+
+        private void MaintenanceStateUpdate() //no way to recover from this state in the simulation
+        {
+            TargetSpeed = 0;
+        }
+
+        private void EmergencyStopRequested()
+        {
+            Debug.WriteLine("Emergency stop requested!");
+            State = PlcState.EmergencyStop;
+        }
+    }
+}
